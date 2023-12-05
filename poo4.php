bca64e987688f8e311d97dd33bf2661552947e11
Bouba <boubacar.konate@outlook.fr>
2023-12-05 12:06:48 +0100

    poo4.php construc  ok

--- poo4.php
+++ poo4.php
@@ -1,9 +1,19 @@
 <?php
 class hello{
+
+    //construc   => méthode magique
+    public function __construct()
+    {
+        echo "Construct is working";                
+    }
  
     //method
     public function formateur(){
         echo "Hello Ganesh";
     }
 }
+
+$formateur = new hello();           //j'accède au constructeur juste en instanciant la class
+echo "<br>";
+echo $formateur->formateur();       //à l'inverse, pour utiliser cette fonction, je dois aller la chercher
 
