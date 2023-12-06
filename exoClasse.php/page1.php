94a3751fbc57258854f2638d7b86ea6fdae5d2de
Bouba <boubacar.konate@outlook.fr>
2023-12-06 11:50:12 +0100

    POO exoClasse.php page1  ok

--- exoClasse.php/page1.php
+++ exoClasse.php/page1.php
@@ -15,15 +15,15 @@ class Personnage{
     public $sexe = true;
     public $experience = 23;
 
-    public function afficher_info_template(){
+    public function afficher_info_template(){                   //méthode1 déclarée
         echo "<table><tr><td>";
         echo " <img src = 'images/$this->img'>";
         echo "</td><td>";
-        echo $this->afficher_caracteristiques();                //méthode appelée
+        echo $this->afficher_caracteristiques();                //méthode2 appelée
         echo "</td></tr></table>";
     }
 
-    function afficher_caracteristiques(){                          //méthode déclarée
+    function afficher_caracteristiques(){                          //méthode2 déclarée
         echo "Nom : ".$this->nom."<br>";
         echo "Age : ".$this->age." ans <br>";
         if ($this->sexe) {                                      //condition créée car sexe est true ou false
@@ -31,12 +31,21 @@ class Personnage{
         }else {
             echo "Sexe :femme <br>";
         }
-        echo "Nom : ".$this->experience." ans <br>";
+        echo "Expériences : ".$this->experience." ans <br>";
     }
 }  
 
-$perso = new Personnage();
-$perso->afficher_info_template()
+$perso1 = new Personnage();
+$perso1->afficher_info_template();                        //méthode1 appelée
+echo "<br>";
+
+$perso2 = new Personnage();
+$perso2->nom ="Sarah";
+$perso2->age = 28;
+$perso2->sexe = false;
+$perso2->experience=8;
+$perso2->img="b.PNG";
+$perso2->afficher_info_template();
 
 ?>
     
