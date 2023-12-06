e7c69183cd2c32c88f57d14ead37c85b92e9f7c9
Bouba <boubacar.konate@outlook.fr>
2023-12-06 15:45:42 +0100

    POO exoClasse.php page4 require classes et page5 getter/setter  ok

--- exoClasse.php/page4.php
+++ exoClasse.php/page4.php
@@ -10,6 +10,10 @@ include("include/header.php");
 require "classes/personnage2.class.php";
                                                     //stactic method
     $perso = new Personnage2("Bobby", 25, "c.PNG", Personnage2::HOMME, 2);
+    $change_nom = "Mouss";
+    $perso->setNom($change_nom);                //modification du nom avec le set
+    $change_age = 30;
+    $perso->setAge($change_age);                //modification de l'âge avecle set
     echo $perso->afficher_info_template();
 
     $perso1 = new Personnage2("Patricia", 45, "b.PNG", Personnage2::FEMME, 15);
