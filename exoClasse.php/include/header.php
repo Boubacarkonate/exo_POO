667d41a2be1971f55d39b46a16701ab2adb86168
Bouba <boubacar.konate@outlook.fr>
2023-12-06 12:19:17 +0100

    POO exoClasse.php page2 __construct  ok

--- exoClasse.php/include/header.php
+++ exoClasse.php/include/header.php
@@ -3,6 +3,7 @@
             <ul>
                 <li><a href="index.php">Accueil</a></li>
                <li><a href="page1.php">POO 1</a></li>
+               <li><a href="page2.php">POO 2</a></li>
             </ul>
         </nav>
     </header>
